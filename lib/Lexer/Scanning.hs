f2ae13430bfa7be576a0d6b986a94bf8a126c556
Alexandra DeWit <alexa.dewit@gmail.com>
2025-10-08 12:47:02 -0400

    empty function for later

---
+++ lib/Lexer/Scanning.hs
@@ -0,0 +1,10 @@
+module Lexer.Scanning where
+
+import qualified Data.Sequence as Seq
+import Lexer.Types (Token)
+import Protolude (Text)
+
+-- Placeholder implementation
+-- Actual scanning logic to be implemented
+scanTokens :: Text -> Seq.Seq Token
+scanTokens _input = Seq.empty
